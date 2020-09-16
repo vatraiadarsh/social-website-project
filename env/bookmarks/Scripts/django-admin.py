2f0d929cb55ade44140f762f36704490eea6ca21
Vatraiadarsh <vatraiadarsh@gmail.com>
2020-09-17 08:37:06 +1000

    Project Starter

---
+++ env/bookmarks/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\vatra\desktop\social website project\env\bookmarks\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
